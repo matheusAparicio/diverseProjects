ef6a6d575044473c5cb4c53a3a336d85d417b8ef
matheusAparicio <matheusilva-m@outlook.com>
2021-12-17 11:33:44 -0300

    adicao dao classe 'Employee'

---
+++ C#/diverseProjects/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diverseProjects
+{
+    internal class Employee
+    {
+        private int _id;
+        private string _name;
+        private double _salary;
+
+        public Employee(int id, string name, double salary) {
+            _id = id;
+            _name = name;
+            _salary = salary;
+        }
+
+        public int GetId() {
+            return _id;
+        }
+
+        public string GetName() {
+            return (_name ?? "");
+        }
+
+        public double GetSalary() {
+            return _salary;
+        }
+
+        public void RaiseSalary(double percentage) {
+            _salary = _salary + (_salary * (percentage / 100));
+        }
+    }
+}
